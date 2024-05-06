fd965ffdcf943e8026e0eedcd9c69232f096652c
Gabriel <gabriel.houth@gmail.com>
2024-05-06 14:51:58 -0300

    initial commit

---
+++ src/main/java/com/gsa/picpaydesafiobackend/PicpayDesafioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gsa.picpaydesafiobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayDesafioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayDesafioBackendApplication.class, args);
+	}
+
+}
