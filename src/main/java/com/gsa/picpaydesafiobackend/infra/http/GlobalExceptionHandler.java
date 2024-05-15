840964177e6baddde32bbead95b3548480b2d33b
Gabriel <gabriel.houth@gmail.com>
2024-05-15 17:49:03 -0300

    feat: add exception handler when no resource is found

--- src/main/java/com/gsa/picpaydesafiobackend/infra/http/GlobalExceptionHandler.java
+++ src/main/java/com/gsa/picpaydesafiobackend/infra/http/GlobalExceptionHandler.java
@@ -4,8 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 
+import com.gsa.picpaydesafiobackend.infra.http.responses.NotFound;
 import com.gsa.picpaydesafiobackend.infra.http.responses.BadRequest;
 import com.gsa.picpaydesafiobackend.infra.http.responses.HttpResponse;
 import com.gsa.picpaydesafiobackend.infra.http.responses.ResponseError;
@@ -18,6 +20,13 @@ public class GlobalExceptionHandler {
     var responseError = new ResponseError("The request body is invalid", "Please check the request body");
     var BadRequest = new BadRequest(responseError);
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(BadRequest);
+  }
+
+  @ExceptionHandler(NoResourceFoundException.class)
+  public ResponseEntity<HttpResponse> handle(NoResourceFoundException exception) {
+    var responseError = new ResponseError("Resource not found", "The resource you are looking for does not exist");
+    var notFound = new NotFound(responseError);
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(notFound);
   }
 
   @ExceptionHandler(Exception.class)
